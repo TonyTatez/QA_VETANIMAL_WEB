0600d70db077fd762f66f488daa0ae660e2077ed
Martial Tatez <josemariavaldezmorales99@gmail.com>
2024-01-21 21:05:19 -0500

    WebViewHC

---
+++ ConautoDT.Web/Controllers/WebViewHCController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VET_ANIMAL.WEB.Controllers
+{
+    public class WebViewHCController : Controller
+    {
+        // GET: WebViewHCController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: WebViewHCController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: WebViewHCController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: WebViewHCController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WebViewHCController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: WebViewHCController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WebViewHCController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: WebViewHCController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
